bbc70425e8cce641daee8572fd54f29ea1e490b6
Bruno Oliveira <bfurlaneto@gmail.com>
2023-07-03 21:46:03 -0300

    chore: initial commit

---
+++ src/test/java/br/com/bfo/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bfo.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
