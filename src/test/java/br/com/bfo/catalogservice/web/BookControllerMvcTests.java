53a44b046efb8a84c52b9842f512f1f9feee3f5a
Bruno Oliveira <bfurlaneto@gmail.com>
2023-07-10 20:16:45 -0300

    feat: create tests

--- src/test/java/br/com/bfo/catalogservice/web/BookControllerMvcTests.java
+++ src/test/java/br/com/bfo/catalogservice/web/BookControllerMvcTests.java
@@ -10,12 +10,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @WebMvcTest(BookController.class)
 class BookControllerMvcTests {
+
     @Autowired
     private MockMvc mockMvc;
+
     @MockBean
     private BookService bookService;
+
     @Test
     void whenGetBookNotExistingThenShouldReturn404() throws Exception {
         String isbn = "73737313940";
--- src/test/java/br/com/bfo/catalogservice/web/BookJsonTests.java
+++ src/test/java/br/com/bfo/catalogservice/web/BookJsonTests.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import static org.assertj.core.api.Assertions.assertThat;
+
 @JsonTest
 class BookJsonTests {
+
     @Autowired
     private JacksonTester<Book> json;
+
     @Test
     void testSerialize() throws Exception {
         var book = new Book("1234567890", "Title", "Author", 9.90);
@@ -23,6 +26,7 @@ class BookJsonTests {
         assertThat(jsonContent).extractingJsonPathNumberValue("@.price")
                 .isEqualTo(book.price());
     }
+
     @Test
     void testDeserialize() throws Exception {
         var content = """
